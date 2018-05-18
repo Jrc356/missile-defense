adf71aa2a981590d47b0085cec608bea0f95f438
Jonathan Corbin <jon.corbin8@gmail.com>
2018-05-18 19:05:41 -0400

    Create model class and add to flask app

--- model/app.py
+++ model/app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, request
+from model import Model
 
+print("Creating Model.", end="")
+print(".", end="")
+print(".")
+model = Model(5)
+print("Model Created...")
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
-    return "Hello World"
-
+    params = request.args.get("data")
+    params = params.split('+')
+    print(type(params))
+    print(params)
+    return str(params)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
